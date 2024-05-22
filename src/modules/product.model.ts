b811e2df189eaabed881b2f44dfae991549847a1
ripon4521 <riponakondo4521@gmail.com>
2024-05-22 10:52:11 +0600

    first commit

---
+++ src/modules/product.model.ts
@@ -0,0 +1,28 @@
+
+import { model, Schema } from 'mongoose';
+import { Product } from './product/product.interface';
+
+
+const productSchema = new Schema<Product>({
+    name: {type: String , required: true},
+    description: {type: String, required: true},
+    price: {type: Number , required: true},
+    category: {type: String , required: true},
+    tags:{type: [String] , required: true},
+    variants:[
+        {
+            type:{type: String , required: true},
+            value: {type: String , required: true}
+        },
+        {
+            type: {type: String , required: true},
+            value:{type: String , required: true}
+        }
+    ],
+    inventory : {
+        quantity: {type: Number , required: true},
+        inStock: {type: Boolean , required: true}
+    }
+});
+
+export const StudentModel = model<Product>('Product', productSchema);
---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,24 @@
+
+  export type Product = {
+    name: string,
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants:[
+        {
+            type: string;
+            value: string;
+        },
+        {
+            type: string;
+            value: string;
+        }
+    ];
+    inventory : {
+        quantity: number;
+        inStock: boolean;
+    }
+    
+  };
+  
